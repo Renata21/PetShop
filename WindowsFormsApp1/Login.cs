b35cfaa826c31b1dbed172608c77c8fa2b9c3733
Cadi <Cadi@DESKTOP-K6QA986>
2022-04-17 20:38:42 +0300

    modificat con-ul

--- WindowsFormsApp1/Login.cs
+++ WindowsFormsApp1/Login.cs
@@ -12,8 +12,9 @@ namespace WindowsFormsApp1
             
         }
         
-       SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\PetShopDb.mdf;Integrated Security=True;Connect Timeout=30");
+       SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\PetShopDb.mdf;Integrated Security=True;Asynchronous Processing=True;User Instance=False;Context Connection=False");
         SqlCommand cmd;
+        
         SqlDataReader dr;
         private void pictureBox2_Click(object sender, EventArgs e)
         {
